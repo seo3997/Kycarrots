ca61316d4cb8fde8bba430ed6ccbf54cbbc7068b
soohyunSeo <seo3997@gmail.com>
2026-04-04 13:37:55 +0900

    firebase 설정변경

--- src/main/java/com/whomade/kycarrots/config/FirebaseConfig.java
+++ src/main/java/com/whomade/kycarrots/config/FirebaseConfig.java
@@ -4,29 +4,34 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config.path}")
+    private Resource firebaseConfigResource;
+
     @PostConstruct
     public void init() {
         try {
-            //FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-service-account.json");
-            InputStream serviceAccount = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("firebase/firebase-service-account.json");
+            if (firebaseConfigResource == null || !firebaseConfigResource.exists()) {
+                return;
+            }
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+            try (InputStream serviceAccount = firebaseConfigResource.getInputStream()) {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
+                if (FirebaseApp.getApps().isEmpty()) {
+                    FirebaseApp.initializeApp(options);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException("Firebase 초기화 실패", e);
